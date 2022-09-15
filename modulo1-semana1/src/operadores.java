7edf195a1a294bb691d66e244506b3f8bc2884d7
marina-hoffmann <marina.hoffmann@aurum.com.br>
2022-09-15 09:51:32 -0300

    exemplos de operadores e desafio de calculo de perimetro

---
+++ modulo1-semana1/src/operadores.java
@@ -0,0 +1,17 @@
+public class operadores {
+    public static void main(String[] args) {
+        int resultado = 3 % 2;
+        System.out.println("O resultado é " + resultado);
+
+        String nome = "Tiago";
+        nome = "MArina";
+
+        float salario = 1200.00F;
+        float salarioReajustado = salario + 200F;
+        System.out.println(salarioReajustado);
+
+        int diaDeSemana = 4;
+        boolean ehFimDeSemana = diaDeSemana == 7;
+        System.out.println(ehFimDeSemana);
+    }
+}
---
+++ modulo1-semana1/src/perimetro.java
@@ -0,0 +1,11 @@
+public class perimetro {
+    public static void main(String[] args) {
+        // C = 2 * pi * r
+
+        float pi = 3.14F;
+        float r = 2F;
+        float c = 2 * pi * r;
+
+        System.out.println("O resultado do perímetro é " + c + " cm.");
+    }
+}
