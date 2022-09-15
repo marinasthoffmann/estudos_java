ef68dbb72952d2caaea85cdb70acf9fb7f69a4f1
marina-hoffmann <marina.hoffmann@aurum.com.br>
2022-09-15 09:09:20 -0300

    Cria estrutura do projeto java

---
+++ modulo1-semana1/src/meuprimeiroprograminha.java
@@ -0,0 +1,15 @@
+public class meuprimeiroprograminha {
+    public static void main(String[] args) {
+
+        // Esse é um comentário de linha
+        /* este é um comentário de bloco
+        posso comentar quantas linhas quiser
+         */
+
+        System.out.println("Olá, mundo!");
+        System.out.println("Olá de novo!");
+
+
+
+    }
+}
---
+++ modulo1-semana1/src/minhasvariaveis.java
@@ -0,0 +1,17 @@
+public class minhasvariaveis {
+    public static void main(String[] args) {
+//        String nome = "Marina";
+        Integer idade = 18;
+        float salario = 5005.50F;
+        boolean ehFimDeSemana = false;
+        Long valorNaoAtribuido = null;
+
+        System.out.println("nome");
+//        System.out.println(nome);
+
+        String nome = "Marina";
+        String sobrenome = "Hoffmann";
+
+        System.out.println(nome + " " + sobrenome);
+    }
+}
