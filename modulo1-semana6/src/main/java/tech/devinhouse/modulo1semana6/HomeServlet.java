788fc8109a1ab2149abede4d74f63c1308752fff
marina-hoffmann <marina.hoffmann@aurum.com.br>
2022-10-17 22:09:47 -0300

    aula 17/10 - introdução servlet

---
+++ modulo1-semana6/src/main/java/tech/devinhouse/modulo1semana6/HomeServlet.java
@@ -0,0 +1,20 @@
+package tech.devinhouse.modulo1semana6;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("Tudo funcionando corretamente! \\o/");
+    }
+}
