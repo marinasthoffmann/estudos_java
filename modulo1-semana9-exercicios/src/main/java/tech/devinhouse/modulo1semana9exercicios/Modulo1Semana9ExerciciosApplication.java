a16ddefb34a47f9b570aad7ade41930674ea34c1
marina-hoffmann <marina.hoffmann@aurum.com.br>
2022-11-13 06:53:32 -0300

    exercicio 1

---
+++ modulo1-semana9-exercicios/src/main/java/tech/devinhouse/modulo1semana9exercicios/Modulo1Semana9ExerciciosApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.modulo1semana9exercicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo1Semana9ExerciciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modulo1Semana9ExerciciosApplication.class, args);
+    }
+
+}
---
+++ modulo1-semana9-exercicios/src/test/java/tech/devinhouse/modulo1semana9exercicios/Modulo1Semana9ExerciciosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.modulo1semana9exercicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo1Semana9ExerciciosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
