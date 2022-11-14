098e3b55efe354fd1d9d78e299682c777b3ff781
marina-hoffmann <marina.hoffmann@aurum.com.br>
2022-11-14 20:31:26 -0300

    exercicio 1

---
+++ modulo1-semana10-copadomundo/src/main/java/tech/devinhouse/modulo1semana10copadomundo/Modulo1Semana10CopadomundoApplication.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.modulo1semana10copadomundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo1Semana10CopadomundoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modulo1Semana10CopadomundoApplication.class, args);
+    }
+
+}
---
+++ modulo1-semana10-copadomundo/src/test/java/tech/devinhouse/modulo1semana10copadomundo/Modulo1Semana10CopadomundoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.modulo1semana10copadomundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo1Semana10CopadomundoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
